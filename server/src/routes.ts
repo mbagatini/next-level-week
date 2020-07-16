f2e1fa87b86d7eeb12f9e3fe66cc7d58babc7066
Morgana Bagatini <morganabagatini@gmail.com>
2020-07-15 21:27:46 -0300

    Validações com o celebrate

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import multer from "multer";
+import { celebrate, Joi } from 'celebrate';
 
 import multerConfig from './config/multer';
 
@@ -20,6 +21,20 @@ routes.get("/items", itemsController.index);
 routes.get("/points", pointsController.index);
 routes.get("/points/:id", pointsController.show);
 
-routes.post("/points", upload.single('image'), pointsController.create);
+routes.post("/points",
+    upload.single('image'),
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.string().required(),
+            longitude: Joi.string().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required(),
+        })
+    }, { abortEarly: false }),
+    pointsController.create);
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
+import { errors } from 'celebrate';
 
 import routes from "./routes";
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 // Disponibiliza as imagens da pasta upload
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
